e06f9f017553aec70e90b8dabba3b1be3ff33cea
emeijdam@ <emeijdam@dasc.nl>
2023-12-21 13:56:50 +0100

    yarn

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
 
   return (
     <div className="App">
+        <div style={{backgroundColor: "red", display: 'flex',  justifyContent: 'center', gap: '12px'}}>
+          <button style={{}}>CLICK ME1</button>
+          <button style={{}}>CLICK ME2</button>
+        </div>
         <NodeCanvas width={window.innerWidth} height={window.innerHeight}/>
     </div>
   );
--- src/nodeCanvas/NodeCanvas.tsx
+++ src/nodeCanvas/NodeCanvas.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 
+
+enum ENodeTypes {
+    SOURCE,
+    TRANSFORM,
+    TERMINAL
+}
 interface INode {
     name: string,
-    type: string
+    type: ENodeTypes
 }
 
 interface NodeCanvasProps {
@@ -10,23 +16,53 @@ interface NodeCanvasProps {
     height: number;
 }
 
+const mySourceNode: INode = { name: 'input1', type: ENodeTypes.SOURCE }
+const myTransformNode: INode = { name: 'input1', type: ENodeTypes.TRANSFORM }
+const myTerminalNode: INode = { name: 'input1', type: ENodeTypes.TERMINAL }
+
+
 const NodeCanvas = (props: NodeCanvasProps) => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
-    const [canvasNodes, setCanvasNodes] = useState<INode[]>([])
+    const [canvasNodes, setCanvasNodes] = useState<INode[]>([mySourceNode, myTransformNode, myTerminalNode])
 
     useEffect(() => {
-        const mynode: INode = { name: 'input1', type: "source" }
-        setCanvasNodes([...canvasNodes, mynode])
 
         if (canvasRef.current) {
             const canvas = canvasRef.current;
             const canvasContext = canvas.getContext('2d');
+
+
+            const startpos = { startX: 100, startY: 100 }
+
             if (canvasContext) {
+                canvasContext.clearRect(0, 0, canvas.width, canvas.height);
                 for (const node of canvasNodes) {
-                    if (node.type === 'source') {
-                        canvasContext.fillRect(100, 100, 100, 100)
+
+                    if (node.type === ENodeTypes.SOURCE) {
+                        canvasContext.beginPath();
+                        canvasContext.arc(startpos.startX, startpos.startY, 50, 0, 2 * Math.PI);
+                        canvasContext.stroke()
                     }
+
+                    else if (node.type === ENodeTypes.TRANSFORM) {
+                        canvasContext.beginPath();
+                        canvasContext.rect(startpos.startX - 50, startpos.startY -50, 100, 100)
+                        canvasContext.stroke()
+                    }
+
+                    else if (node.type === ENodeTypes.TERMINAL) {
+                        canvasContext.beginPath();
+                        canvasContext.moveTo(startpos.startX - 50, startpos.startY);
+                        canvasContext.lineTo(startpos.startX, startpos.startY - 50);
+                        canvasContext.lineTo(startpos.startX + 50, startpos.startY);
+                        canvasContext.lineTo(startpos.startX, startpos.startY + 50);
+                        canvasContext.lineTo(startpos.startX -50, startpos.startY);
+                        canvasContext.stroke()
+                    }
+
+                    startpos.startX = startpos.startX + 175
                 }
+
             }
         }
     }, [canvasNodes])
